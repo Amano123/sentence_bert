438f48154762b7ea75607a81fe4832d6a5666967
yoo <gp17a003@oecu.jp>
2022-07-08 13:53:33 +0900

    [add]発話者ペアの分散を計算

--- workspace/vector_visualization.py
+++ workspace/vector_visualization.py
@@ -3,10 +3,11 @@ import pandas as pd
 import numpy as np
 from operator import itemgetter
 import torch
+import itertools
 #%%
 # df = pd.read_pickle("/home/yo/workspace/話者毎に色分け-最終盤/2019-02-20/2019-02-20.pkl")
-df = pd.read_pickle("/home/yo/workspace/話者毎に色分け-最終盤/2019-02-25/2019-02-25.pkl")
-# df = pd.read_pickle("/home/yo/workspace/話者毎に色分け-最終盤/2020-03-12/2020-03-12.pkl")
+# df = pd.read_pickle("/home/yo/workspace/話者毎に色付け/2019-02-25/2019-02-25.pkl")
+df = pd.read_pickle("/home/yo/workspace/話者毎に色付け/2020-02-20/2020-02-20.pkl")
 
 # %%
 sentence_embeddings = df["sentence_embeddings"][0]
@@ -31,5 +32,18 @@ for spe in set(speaker):
     distributed_cls_emb = np.var(np.array(filter_cls_emb))
     print(
         f"{spe}, \n\tsentence distributed: {distributed_sentence_emb}, \n\tCLS distributed     : {distributed_cls_emb}"
+        )
+# %%
+for spe1, spe2 in list(itertools.combinations(set(speaker), 2)):
+    print(spe1, spe2)
+    filter_index = spe_index[spe1] + spe_index[spe2]
+
+    filter_sentence_emb = itemgetter(filter_index)(sentence_embeddings)
+    filter_cls_emb = itemgetter(filter_index)(torch.Tensor(cls_token_embeddings))
+
+    distributed_sentence_emb = np.var(np.array(filter_sentence_emb))
+    distributed_cls_emb = np.var(np.array(filter_cls_emb))
+    print(
+        f"{spe1, spe2}, \n\tsentence distributed: {distributed_sentence_emb}, \n\tCLS distributed     : {distributed_cls_emb}"
         )
 # %%
