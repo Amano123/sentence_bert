b2167db68b538f25a6f9e30b4b798a090456488b
yoo <mi21a001@oecu.jp>
2022-07-20 11:01:14 +0900

    [add]

--- workspace/kKMeans_visualization.py
+++ workspace/kKMeans_visualization.py
@@ -1,6 +1,9 @@
 #%%
 import pandas as pd
 import numpy as np
+from numpy import dot
+from numpy.linalg import norm
+
 from operator import itemgetter
 import torch
 import itertools
@@ -10,20 +13,22 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 import collections
 #%%
-# df = pd.read_pickle("/home/yo/workspace/話者毎に色分け-最終盤/2019-02-20/2019-02-20.pkl")
-# df = pd.read_pickle("/home/yo/workspace/話者毎に色付け/2019-02-25/2019-02-25.pkl")
+## データセット指定
 date_str = "2019-02-20"
 df = pd.read_pickle(f"/home/yo/workspace/話者毎に色付け-2022-07-07/{date_str}/{date_str}.pkl")
 
 # %%
+## T-sneのデータ抽出
 cls_tsne_emnedding_df = pd.DataFrame(df["cls_tsne_emnedding"][0])
 sentence_tsne_emnedding_df = pd.DataFrame(df["sentence_tsne_emnedding"][0])
 
+# emneddingを抽出
 sentence_embeddings_df = pd.DataFrame(df["sentence_embeddings"][0])
 cls_token_embeddings_df = pd.DataFrame(df["cls_token_embeddings"][0])
 utterance = df["utterance"][0]
 
 #%%
+# Topic データを読み込む
 topic_df = pd.read_csv(f"/home/yo/workspace/{date_str}-document_topic.csv")
 topic_df["document_topic"] = topic_df.iloc[:, 1:].idxmax(axis=1)
 
@@ -57,29 +62,40 @@ for (index, score), utte in zip(enumerate(topic_df.document_topic), utterance):
 
 # %%
 cls_tsne_emnedding_df["topic"] = topic_df["document_topic"]
-cls_tsne_emnedding_df["topic_filter"] = utterance_topic_list
-
+# cls_tsne_emnedding_df["topic_filter"] = utterance_topic_list
+cls_tsne_emnedding_df["cluster_label"] = cls_token_cluster_labels
 
 sentence_tsne_emnedding_df["topic"] = topic_df["document_topic"]
-sentence_tsne_emnedding_df["topic_filter"] = utterance_topic_list
-
+# sentence_tsne_emnedding_df["topic_filter"] = utterance_topic_list
+sentence_tsne_emnedding_df["cluster_label"] = sentence_cluster_labels
 
 # %%
 def plot_function(enbedding_df, label):
-    _df = enbedding_df[enbedding_df["topic_filter"] == True]
+    _df = enbedding_df#[enbedding_df["topic_filter"] == True]
     topic_num = len(set(enbedding_df["topic"]))
-    topic_tag = [f"topic{i}" for i in range(1,topic_num +1)]
+    # topic_tag = [f"topic{i}" for i in range(1,topic_num +1)]
     topic_tag = set(enbedding_df["cluster_label"])
 
     fig, ax = plt.subplots(figsize=(8, 8))
     for tag in topic_tag:
-        xy_pickup = _df[_df["topic"] == tag]
+        # xy_pickup = _df[_df["topic"] == tag]
+        xy_pickup = _df[_df["cluster_label"] == tag]
         x = xy_pickup[0]
         y = xy_pickup[1]
         ax.scatter(x, y, s=6, label=tag)   # type: ignore
         ax.legend(loc='center left', bbox_to_anchor=(1., .5))
     plt.savefig(f'{date_str}-{label}.png', bbox_inches='tight')    
 
+#%%
+def sample_similarity(dataset, search_data):
+    result_list = []
+    x = dataset[0]
+    y = dataset[1]
+    for xy in zip(x, y):
+        # print(dataset[d])
+        result = np.dot(list(xy), search_data) / (np.linalg.norm(list(xy)) * np.linalg.norm(search_data))
+        result_list.append(result)
+    return np.argmax(result_list)
 
 # %%
 plot_function(cls_tsne_emnedding_df, "CLS")
